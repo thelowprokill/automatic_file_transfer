18e291c72efc0107f911a55a3c21325de0ef5394
Jon Hull <jonathan.hull@beefnw.com>
2020-11-24 17:53:44 -0800

    exe

--- update_ui.py
+++ update_ui.py
@@ -171,10 +171,12 @@ class ui_handler(QDialog):
         self.ui.setup_ui(self)
         self.show()
         self.log = lw.logwriter()
-        self.config = cl.config_loader(log.write)
-        log.construct(config.LOG_FILE, config.PROGRAM_TITLE, config.VERSION, self.add_text, config.LOG_LEVEL)
-        self.thread = Thread(target = self.start_program)
-        self.thread.start()
+        self.config = cl.config_loader(self.log.write)
+        self.log.construct(self.config.LOG_FILE, self.config.PROGRAM_TITLE, self.config.VERSION, self.add_text, self.config.LOG_LEVEL)
+        self.main_thread = Thread(target = self.start_program)
+        self.main_thread.start()
+        self.update_thread = Thread(target = self.start_update)
+        self.update_thread.start()
 
     def add_text(self, s):
         self.ui.add_text(s)
@@ -191,7 +193,10 @@ class ui_handler(QDialog):
                 pass
 
     def start_program(self):
-        call([self.config.EXE])
+        try:
+            call([self.config.EXE])
+        except:
+            self.log.write(0, "Error: Failed to launch program {}".format(self.config.EXE))
 
     def start_update(self):
         self.trans = tp.transporter(self.log.write, self.config, self.ui.set_mode)
